c9e5fbacd6ced770f67a155d4ac169ea17af94d0
Andreea Roberta Muresan <muresan.andreea16@yahoo.com>
2020-11-27 15:07:48 +0200

    Add project files.

---
+++ Data/Muresan_Andreea_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Andreea_Lab8.Models;
+
+namespace Muresan_Andreea_Lab8.Data
+{
+    public class Muresan_Andreea_Lab8Context : DbContext
+    {
+        public Muresan_Andreea_Lab8Context (DbContextOptions<Muresan_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muresan_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Muresan_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muresan_Andreea_Lab8.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
+
